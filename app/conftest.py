6b89665017cf9def5e7c871cb073241a66ebaff7
Aart Goossens <aart@goossens.me>
2019-10-25 14:19:32 +0200

    Enabling async tests

--- app/conftest.py
+++ app/conftest.py
@@ -2,7 +2,13 @@ import pytest
 import sqlalchemy
 from pathlib import Path
 
+import nest_asyncio
+
 from config import database, metadata, DATABASE_PATH
+
+
+# https://github.com/spyder-ide/spyder/issues/7096#issuecomment-449655308
+nest_asyncio.apply()
 
 
 @pytest.fixture(scope='function', autouse=True)
